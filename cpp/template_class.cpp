b52f0d50d3e884d483b0adbbcbb8e9e9d83952ef
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-02-16 01:54:32 +0530

    added new cpp file

---
+++ cpp/template_class.cpp
@@ -0,0 +1,41 @@
+//Template class
+#include<iostream>
+using namespace std;
+
+template<class T>
+class Arithametic{
+    private:
+        T a;
+        T b;
+        
+    public:
+        Arithametic(T a,T b);
+        T add();
+        T sub();
+};
+template<class T>
+Arithametic<T>::Arithametic(T a,T b){
+    this->a=a;
+    this->b=b;
+}
+template<class T>
+T Arithametic<T>::add(){
+    T c;
+    c=a+b;
+    return c;
+}
+
+template<class T>
+T Arithametic<T>::sub(){
+    T c;
+    c=a-b;
+    return c;
+}
+
+int main(){
+    Arithametic<int> ar(10,5);
+    cout<<ar.add()<<endl;
+    
+    Arithametic<float> ar1(10.323,12.5324);
+    cout<<ar1.add()<<endl<<ar1.sub()<<endl;
+}
